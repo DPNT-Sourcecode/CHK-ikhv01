a1001645ff6546c375e0ee7398ec13fc2d0049d4
sbx_user1051 <sbx_user1051@169.254.2.213>
2025-11-06 17:49:18 +0000

    Thu Nov 06 17:49:18 UTC 2025

--- src/test/kotlin/solutions/CHK/CheckoutSolutionTest.kt
+++ src/test/kotlin/solutions/CHK/CheckoutSolutionTest.kt
@@ -1,10 +1,21 @@
 package solutions.CHK
 
-import org.junit.jupiter.api.Test
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CheckoutSolutionTest {
-    @Test
-    fun `checkout should return `() {
+
+    private fun getInvalidInputs() = listOf("a", "A1", "z", "E", "*", "Aba")
+
+    @ParameterizedTest
+    @MethodSource("getInvalidInputs")
+    fun `checkout should return -1 if any illegal input is provided`(input: String) {
+        val checkout = CheckoutSolution()
+        checkout.checkout(input) shouldBe -1
+
     }
 
 }
