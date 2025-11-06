85491991b931fe7a3e4cdc6374694ded4583a06b
sbx_user1051 <sbx_user1051@169.254.2.213>
2025-11-06 17:58:18 +0000

    Thu Nov 06 17:58:18 UTC 2025

--- src/test/kotlin/solutions/CHK/CheckoutSolutionTest.kt
+++ src/test/kotlin/solutions/CHK/CheckoutSolutionTest.kt
@@ -3,6 +3,7 @@ package solutions.CHK
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -15,7 +16,22 @@ class CheckoutSolutionTest {
     fun `checkout should return -1 if any illegal input is provided`(input: String) {
         val checkout = CheckoutSolution()
         checkout.checkout(input) shouldBe -1
+    }
 
+
+    private fun getSingeItemInputs() = listOf(
+        Arguments.of("A" to 50),
+        Arguments.of("B" to 30),
+        Arguments.of("C" to 20),
+        Arguments.of("D" to 15),
+        Arguments.of("" to 0)
+    )
+
+    @ParameterizedTest
+    @MethodSource("getSingeItemInputs")
+    fun `checkout should calculate totals for single items`(input: String, expected: Int) {
+        val checkout = CheckoutSolution()
+        checkout.checkout(input) shouldBe expected
     }
 
 }
